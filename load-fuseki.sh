5aed577df7d615bec79ce7f983fc3e8bb904e1f4
Loïc Jaouen <jaouen@bluewin.ch>
2022-05-13 10:18:17 +0200

    upload standoff

--- load-fuseki.sh
+++ load-fuseki.sh
@@ -5,29 +5,33 @@ ARGHOST=${2:-http://localhost:3030}
 ARGREPO=knora-test
 
 
-ARGTTL=roud-admin.ttl
-ARGGRAPH=http://www.knora.org/data/admin
-curl -u $USERPWD  -H "Content-Type:text/turtle; charset=utf-8" --data-binary @${ARGTTL} -X POST ${ARGHOST}/${ARGREPO}\?graph\="${ARGGRAPH}"
+# commented out to load standoff
+# ARGTTL=roud-admin.ttl
+# ARGGRAPH=http://www.knora.org/data/admin
+# curl -u $USERPWD  -H "Content-Type:text/turtle; charset=utf-8" --data-binary @${ARGTTL} -X POST ${ARGHOST}/${ARGREPO}\?graph\="${ARGGRAPH}"
 
-ARGTTL=roud-permissions.ttl
-ARGGRAPH=http://www.knora.org/data/permissions
-curl -u $USERPWD  -H "Content-Type:text/turtle; charset=utf-8" --data-binary @${ARGTTL} -X POST ${ARGHOST}/${ARGREPO}\?graph\="${ARGGRAPH}"
+# commented out to load standoff
+# ARGTTL=roud-permissions.ttl
+# ARGGRAPH=http://www.knora.org/data/permissions
+# curl -u $USERPWD  -H "Content-Type:text/turtle; charset=utf-8" --data-binary @${ARGTTL} -X POST ${ARGHOST}/${ARGREPO}\?graph\="${ARGGRAPH}"
 
-ARGTTL=roud-onto-download-dasch.ttl
-ARGGRAPH=http://www.knora.org/ontology/0112/roud-oeuvres
-curl -u $USERPWD  -H "Content-Type:text/turtle; charset=utf-8" --data-binary @${ARGTTL} -X POST ${ARGHOST}/${ARGREPO}\?graph\="${ARGGRAPH}"
+# commented out to load standoff
+# ARGTTL=roud-onto-download-dasch.ttl
+# ARGGRAPH=http://www.knora.org/ontology/0112/roud-oeuvres
+# curl -u $USERPWD  -H "Content-Type:text/turtle; charset=utf-8" --data-binary @${ARGTTL} -X POST ${ARGHOST}/${ARGREPO}\?graph\="${ARGGRAPH}"
 
 # ARGTTL=roud-onto.ttl
 # ARGGRAPH=http://www.knora.org/ontology/0112/roud-oeuvres
 # curl -u $USERPWD  -H "Content-Type:text/turtle; charset=utf-8" --data-binary @${ARGTTL} -X POST ${ARGHOST}/${ARGREPO}\?graph\="${ARGGRAPH}"
 
-# ARGTTL=roud-standoff.ttl
-# ARGGRAPH=http://www.knora.org/ontology/0112/roud-oeuvres
-# curl -u $USERPWD  -H "Content-Type:text/turtle; charset=utf-8" --data-binary @${ARGTTL} -X POST ${ARGHOST}/${ARGREPO}\?graph\="${ARGGRAPH}"
-
-ARGTTL=roud-data-download-dasch.ttl
-ARGGRAPH=http://www.knora.org/data/0112/roud-oeuvres
+ARGTTL=./mapping/roudMapping.ttl
+ARGGRAPH=http://www.knora.org/ontology/0112/roud-oeuvres
 curl -u $USERPWD  -H "Content-Type:text/turtle; charset=utf-8" --data-binary @${ARGTTL} -X POST ${ARGHOST}/${ARGREPO}\?graph\="${ARGGRAPH}"
+
+# commented out to load standoff
+# ARGTTL=roud-data-download-dasch.ttl
+# ARGGRAPH=http://www.knora.org/data/0112/roud-oeuvres
+# curl -u $USERPWD  -H "Content-Type:text/turtle; charset=utf-8" --data-binary @${ARGTTL} -X POST ${ARGHOST}/${ARGREPO}\?graph\="${ARGGRAPH}"
 
 # ARGTTL=roud-data-lists.ttl
 # ARGGRAPH=http://www.knora.org/data/0112/roud-oeuvres
