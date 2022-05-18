4e94ff75b1f81f68070484cb26ff728a04ce94df
Loïc Jaouen <jaouen@bluewin.ch>
2022-05-18 09:13:13 +0200

    make repo configurable (has changed on recent dasch set-ups)

--- load-fuseki.sh
+++ load-fuseki.sh
@@ -2,20 +2,20 @@
 USERPWD=${1:-admin:test}
 #ARGHOST=http://db-sipipv.unil.ch:3030
 ARGHOST=${2:-http://localhost:3030}
-ARGREPO=knora-test
+ARGREPO=${3:-knora-test}
 
 
-# commented out to load standoff
+# # commented out to load standoff
 # ARGTTL=roud-admin.ttl
 # ARGGRAPH=http://www.knora.org/data/admin
 # curl -u $USERPWD  -H "Content-Type:text/turtle; charset=utf-8" --data-binary @${ARGTTL} -X POST ${ARGHOST}/${ARGREPO}\?graph\="${ARGGRAPH}"
 
-# commented out to load standoff
+# # commented out to load standoff
 # ARGTTL=roud-permissions.ttl
 # ARGGRAPH=http://www.knora.org/data/permissions
 # curl -u $USERPWD  -H "Content-Type:text/turtle; charset=utf-8" --data-binary @${ARGTTL} -X POST ${ARGHOST}/${ARGREPO}\?graph\="${ARGGRAPH}"
 
-# commented out to load standoff
+# # commented out to load standoff
 # ARGTTL=roud-onto-download-dasch.ttl
 # ARGGRAPH=http://www.knora.org/ontology/0112/roud-oeuvres
 # curl -u $USERPWD  -H "Content-Type:text/turtle; charset=utf-8" --data-binary @${ARGTTL} -X POST ${ARGHOST}/${ARGREPO}\?graph\="${ARGGRAPH}"
@@ -28,7 +28,7 @@ ARGTTL=./mapping/roudMapping.ttl
 ARGGRAPH=http://www.knora.org/ontology/0112/roud-oeuvres
 curl -u $USERPWD  -H "Content-Type:text/turtle; charset=utf-8" --data-binary @${ARGTTL} -X POST ${ARGHOST}/${ARGREPO}\?graph\="${ARGGRAPH}"
 
-# commented out to load standoff
+# # commented out to load standoff
 # ARGTTL=roud-data-download-dasch.ttl
 # ARGGRAPH=http://www.knora.org/data/0112/roud-oeuvres
 # curl -u $USERPWD  -H "Content-Type:text/turtle; charset=utf-8" --data-binary @${ARGTTL} -X POST ${ARGHOST}/${ARGREPO}\?graph\="${ARGGRAPH}"
