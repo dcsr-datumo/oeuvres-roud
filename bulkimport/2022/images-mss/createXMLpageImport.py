ad7c400903149ea7ab8396a435abd7a3b842fe0d
ES <spadinielena@gmail.com>
2022-08-06 15:03:56 +0200

    add transformation script and clean file list

---
+++ bulkimport/2022/images-mss/createXMLpageImport.py
@@ -0,0 +1,92 @@
+###
+### CREATE XML FOR BULK IMPORT OF SCANS
+###
+
+
+from xml.etree import ElementTree as ET
+import csv
+import os
+
+### Open list of tiff to be imported
+# test with 'list-tiff-mss_2import_test.txt'
+listTifMss_file = open('list-tiff-mss_2import.txt', 'r', encoding="utf-8")
+
+listTifMss = listTifMss_file.readlines()
+
+for tif in listTifMss:
+
+    # from CRLR_GR_MS6B110a/CRLR_GR_MS6B110a_1r_1.tif, remove first part and extension > CRLR_GR_MS6B110a_1r_1
+    tif = tif.split('/')[1].split('.')[0] 
+
+    print(tif)
+    
+    ### Building blocks
+    msN = tif[10]  # 6
+    msLet = tif[11]  # B
+    msRest = tif[12:]  # 110a_1r_1.tif
+    msRestShelfmark = msRest.split('_')[0]  # 110a
+    msShelfmark = 'MS' + msN + msLet + msRestShelfmark
+
+    if (msRest.find('couv1') > 0):
+        # example: CRLR_GR_MS6G55a_couv1_1r_1
+        pageName = "première de couverture " + msRest.split('_')[2]
+        pageSeqnum = msRest.split('_')[3]
+    elif (msRest.find('couv4') > 0):
+        # example: CRLR_GR_MS6G55a_couv4_1r_255
+        pageName = "quatrième de couverture " + msRest.split('_')[2]
+        pageSeqnum = msRest.split('_')[3]
+    else:
+        # example: CRLR_GR_MS6B110a_1r_1
+        pageName = "f. " + msRest.split('_')[1]  # 1r
+        pageSeqnum = msRest.split('_')[2]  # 1
+
+
+    # csv downloaded from sparql query (query saved in graphdb). First column: iri, second column: shelfmark 
+    # attention to delimiter for reading csv
+    ### check shelfmark and store IRI in msTarget
+    with open("iri-shelfmark_test.csv", 'r') as csv_iriShelfmark_correspondance:     
+        iriShelfmark_correspondance = csv.reader(csv_iriShelfmark_correspondance, delimiter =',', doublequote=True)
+        for row in iriShelfmark_correspondance:
+            if (msShelfmark == row[1]):
+                msTarget = row[0]
+
+
+        ### Content for the XML
+        pLabel = 'page_CRLR GR MS' + msN + ' ' + msLet + msRestShelfmark + '___' + pageName + '___' + pageSeqnum
+        pId = tif + '.tif'
+        pBitstream = tif + '.tif'
+        pName = pageName
+        pSeqnum = pageSeqnum
+        pMs = msTarget
+
+    ### Write to XML
+    if not os.path.exists('xml'):
+        os.makedirs('xml')
+    o = open('./xml/import'+tif+'.xml', 'w', encoding="utf-8")
+    o.write('<?xml version="1.0" encoding="utf-8"?>'+'\n'+'<knora xmlns="https://dasch.swiss/schema" xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance"'+'\n'+'    xsi:schemaLocation="https://dasch.swiss/schema https://raw.githubusercontent.com/dasch-swiss/dsp-tools/main/knora/dsplib/schemas/data.xsd"'+'\n'+'    shortcode="0112" default-ontology="roud-oeuvres">'+'\n'+'    '+'\n'+'    <permissions id="img-default">'+'\n'+'        <allow group="UnknownUser">V</allow>'+'\n'+'        <allow group="KnownUser">D</allow>'+'\n'+'        <allow group="ProjectAdmin">CR</allow>'+'\n'+'        <allow group="Creator">CR</allow>'+'\n'+'    </permissions>'+'\n')
+
+
+    Resource = ET.Element('resource', attrib={'label':pLabel, 'restype':':Page', 'id':pId, 'permissions':'img-default'})
+
+    Bitstream = ET.SubElement(Resource, 'bitstream', attrib={'permissions':'img-default'})   ## mimetype: png or tiff !!!
+    Bitstream.text = pBitstream
+
+    PageHasNameProp = ET.SubElement(Resource, 'text-prop', attrib={'name':':pageHasName'})
+    PageHasName = ET.SubElement(PageHasNameProp, 'text', attrib={'permissions':'img-default', 'encoding':'utf8'})
+    PageHasName.text = pName
+
+    HasSeqnumProp = ET.SubElement(Resource, 'integer-prop', attrib={'name':':hasSeqnum'})
+    HasSeqnum = ET.SubElement(HasSeqnumProp, 'integer', attrib={'permissions':'img-default'})
+    HasSeqnum.text = pSeqnum
+
+    PartOfMsProp = ET.SubElement(Resource, 'resptr-prop', attrib={'name':':pageIsPartOfManuscript'})
+    PartOfMs = ET.SubElement(PartOfMsProp, 'resptr', attrib={'permissions':'img-default'})
+    PartOfMs.text = pMs
+
+    tree = ET.tostring(Resource, encoding="unicode")
+    o.write('\n''\n'+ tree)
+
+
+## WRITE END OF THE XML FILE
+    o.write('\n'+'</knora>')
+    o.close()
